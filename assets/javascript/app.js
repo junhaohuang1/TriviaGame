6a80b2339ac5f12d26dfdb2b103c767618a521d9
pc <pc>
2017-09-14 22:16:28 -0400

    made some progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,120 @@
-var myQuestions = new Map();
+var questions = [{
+    question: "Who is Boruto's dad?",
+    choices: ["Saskue", "Rock Lee", "Kakashi", "Orochimaru", "Naruto"],
+    correctAnswer: "Naruto"
+}, {
+    question: "Who was Saskue's first kiss?",
+    choices: ["Sakura", "Rock Lee", "Ten Ten", "Ino", "Naruto"],
+    correctAnswer: "Naruto"
+}, {
+    question: "Who is the 7th Hokage?",
+    choices: ["Orochimaru", "Kakashi", "Naruto", "Ino", "Saskue"],
+    correctAnswer: "Naruto"
+}, {
+    question: "Who is 4th Hokage's son?",
+    choices: ["Neji", "Naruto", "Kakashi", "Obito", "Rock Lee"],
+    correctAnswer: "Naruto"
+}, {
+    question: "Who is the current Kyuubi Jinchuriki?",
+    choices: ["Bee", "Naruto", "Saskue", "Boruto", "Sarada"],
+    correctAnswer: "Naruto"
+}];
 
-myQuestions.set("Who is Boruto's dad?", "Naruto");
-myQuestions.set("Who was Saskue's first kiss?", "Naruto");
-myQuestions.set("Who is the 7th Hokage?", "Naruto");
-myQuestions.set("Who is 4th Hokage's son?", "Naruto");
-myQuestions.set("Who is the current Kyuubi Jinchuriki?", "Naruto");
+var images = ["picture1", "picture2", "picture3", "picture4", "picture5", ]
+
+numberRight = 0;
+numberWrong = 0;
+numberUnanswered = 0;
+var userAnswers = [];
 
 
+for (var i = 0; i < questions.length; i++) {
+    var pTag = $("<p>");
+    var form = $("<form class='question'>");
 
+    //set our question
+    pTag.html(questions[i].question);
+    
+    for(var j = 0; j < questions[i].choices.length; j++){
+        
+        var label = $("<label class='radio-inline'>");
 
+        var choice = $("<input type='radio' name='optradio'>");
 
-$(document).ready(function() {
-
-    var isGameStarted = false;
-    var isNewQuestion = false;
-
-    $("#start-button").on('click'function() {
-        isGameStarted = true;
-        isNewQuestion = true;
-        $("#game-area").append("<div>"+ stopwatch.time +"</div>");
-
-    });
-
-    var decrement = function() {
-
+        //set attributes for choice
+        choice.attr("value", questions[i].choices[j]);
+        label.append(choice);
+        label.append(questions[i].choices[j]);
+        form.append(label);
+        pTag.append(form);
     }
-    var stopwatch = {
 
-        time: 30,
-        reset: function() {
-            stopwatch.time = 30;
-            $("#display").html("30");
-
-        },
-
-        start: function() {
+    $('#questionDiv').append(pTag);
 
 
-            if (isGameStarted) {
-                intervalId = setInterval(stopwatch.count, 1000);
-                clockRunning = true;
-
-            }
-
-        },
-        stop: function() {
+} // end for
 
 
-            clearInterval(intervalId);
-            clockRunning = false;
+//Countdown Timer
+var n = 75;
+setTimeout(countDown,1000);
 
-        },
+function countDown(){
+   n--;
+   if(n > 0){
+      setTimeout(countDown,1000);
+   }
 
-        count: function() {
-            stopwatch.time++;
-            var currentTime = stopwatch.timeConverter(stopwatch.time);
-            $("#display").html(currentTime);
+   $("#countdown").html(n);
 
-        },
-
+   if (n === 0){
+    grader();
+   }
 
 
-        timeConverter: function(t) {
+} //end countdown
 
 
-            var minutes = Math.floor(t / 60);
-            var seconds = t - (minutes * 60);
+//Function to grade the results
+function grader(){
 
-            if (seconds < 10) {
-                seconds = "0" + seconds;
-            }
 
-            if (minutes === 0) {
-                minutes = "00";
-            } else if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
+    //Finds questions.
+    var selected = $('.question');
+    $.each(selected, function(){
+        var checkBox = $(this).find("input[type=radio]:checked");
 
-            return minutes + ":" + seconds;
+        if (!checkBox.val()){
+            console.log("unanswered");
+            userAnswers.push("unanswered");
         }
-    };
+        if (checkBox.val()) {
+            console.log(checkBox.val());
+            userAnswers.push(checkBox.val());
+        }
+        console.log(userAnswers);
+    }) //end of each selected function
 
-});
+    //grades the questions
+    for (var i = 0; i < questions.length; i++) {
+        if (userAnswers[i]=="unanswered") {
+            numberUnanswered++;
+        }
+        else {
+
+            if (questions[i].correctAnswer==userAnswers[i]) {
+                numberRight++;
+            }
+            if (questions[i].correctAnswer!=userAnswers[i]){
+                numberWrong++;
+            }
+        }
+        $("#correct").html(numberRight);
+        $("#wrong").html(numberWrong);
+        $("#unanswered").html(numberUnanswered);
+        console.log(numberUnanswered);
+        console.log(numberRight);
+        console.log(numberWrong);
+    }//end for
+
+} // end grader function
